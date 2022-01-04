bd34200b7796653ec7b80a5abb293a8a9f7a3aae
RAN VAKNIN <ranvaknin0@gmail.com>
2022-01-03 21:35:47 -0800

    editing and cleaning up code

--- app.js
+++ app.js
@@ -1,71 +1,78 @@
 // imports \\
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
-const tasks = require('./db');
-const short = require('short-uuid');
-const OktaJwtVerifier = require('@okta/jwt-verifier');
+const tasks = require("./db");
+const uuid = require("short-uuid");
+const OktaJwtVerifier = require("@okta/jwt-verifier");
 
 const oktaJwtVerifier = new OktaJwtVerifier({
-  issuer: 'https://dev-71544132.okta.com/oauth2/default' 
+    issuer: "https://dev-71544132.okta.com/oauth2/default",
 });
 
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json()
+const bodyParser = require("body-parser");
+const jsonParser = bodyParser.json();
 
 // controller \\
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // get all tasks
-app.get('/tasks', (req,res) =>{
-    let newArr = [];
-    [...tasks.values()].forEach(t=>{
-        let owner = obscureEmail(t.email);
-        newArr.push((({id, name,description}) => ({id,name,description,owner}))(t))
-    })
-    res.send(newArr);
+app.get("/tasks", (req, res) => {
+    let obscuredEmailTasks = [...tasks.values()].map((task) => ({
+        id: task.id,
+        name: task.name,
+        description: task.description,
+        owner: obscureEmail(task.email),
+    }));
+    res.send(obscuredEmailTasks);
 });
 
-// add a new task
-app.post('/tasks', jsonParser,  (req,res) =>{
-    oktaJwtVerifier.verifyAccessToken(req.headers.authorization, 'api://default')
-    .then(jwt => {
-        let name = req.body.name;
-        let description = req.body.description;
-        let email = jwt.claims.sub;
-        let id = short.generate();
-        tasks.set(id, {"id": id, "name": name, "description": description,"email":email})
+// Verify that the user is logged in, if they are allow to add a task.
+app.post("/tasks", jsonParser, async (req, res) => {
+    try {
+        // make sure this token came from okta
+        const jwt = await oktaJwtVerifier.verifyAccessToken(req.headers.authorization, "api://default");
+        const name = req.body.name;
+        const description = req.body.description;
+        const email = jwt.claims.sub;
+        const id = uuid.generate();
+        tasks.set(id, { id: id, name: name, description: description, email: email });
         res.status(200);
-    } )
-    .catch(err => console.warn('token failed validation') );
-})
+    } catch (error) {
+        console.warn("token failed validation");
+        res.status(500);
+    }
+});
 
-app.delete('/tasks/:id', (req,res) => {
-        oktaJwtVerifier.verifyAccessToken(req.headers.authorization, 'api://default')
-        .then(jwt => {
-                let taskId = req.params.id;
-                let email = jwt.claims.sub;
-                if(email === tasks.get(taskId).email){
-                    tasks.delete(taskId);
-                    res.status(200).send({"message": "Entry removed successfully", "tasks": tasks});
-                } else {
-                    res.status(403).send({"message": "You can only delete entries created by you."});
-                }
-        })
-        .catch(err => {
-            console.warn('token failed validation');
-            console.log(err);
-        });
-})
+// Verify the user is logged in, only then check that the selected task is owned by the user. if it is then delete it.
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const jwt = await oktaJwtVerifier.verifyAccessToken(req.headers.authorization, "api://default");
+        const taskId = req.params.id;
+        const email = jwt.claims.sub;
+
+        // verify task ownership
+        if (email !== tasks.get(taskId).email) {
+            res.status(403).send({ message: "You can only delete entries created by you." });
+            return;
+        }
+
+        tasks.delete(taskId);
+        res.status(200).send({ message: "Entry removed successfully", tasks: tasks });
+    } catch (error) {
+        console.warn("token failed validation");
+        console.log(err);
+        res.status(500);
+    }
+});
 
 // utility functions
-function obscureEmail(emailString){
-    let splitEmail = emailString.split("@")
+function obscureEmail(emailString) {
+    let splitEmail = emailString.split("@");
     let domain = splitEmail[1];
     let name = splitEmail[0];
-    return  name.substring(0,3).concat("*********@").concat(domain)
+    return name.substring(0, 3).concat("*********@").concat(domain);
 }
 
 app.listen(port);
--- public/index.js
+++ public/index.js
@@ -1,154 +1,170 @@
-
 var animation = bodymovin.loadAnimation({
     // animationData: { /* ... */ },
-    container: document.getElementById('main_img'), // required
-    path: 'animation.json', // required
-    renderer: 'svg', // required
+    container: document.getElementById("main_img"), // required
+    path: "animation.json", // required
+    renderer: "svg", // required
     loop: true, // optional
     autoplay: true, // optional
     name: "Demo Animation", // optional
 });
 
 const oktaSignIn = new OktaSignIn({
-    baseUrl: 'https://dev-71544132.okta.com',
-    redirectUri: 'http://localhost:8080/login/callback',
-    clientId: '0oa3dyffcd08Ne9SE5d7',
+    baseUrl: "https://dev-71544132.okta.com",
+    redirectUri: "http://localhost:8080",
+    clientId: "0oa3dyffcd08Ne9SE5d7",
     authParams: {
         scopes: [],
-        issuer: "https://dev-71544132.okta.com/oauth2/default"
+        issuer: "https://dev-71544132.okta.com/oauth2/default",
     },
-    features:{
-        smsRecovery: true
-    }
+    features: {
+        smsRecovery: true,
+        registration: true,
+    },
 });
 
-let tryButton = document.getElementById("try");
-let tryButton2 = document.getElementById("try2");
-$(tryButton2).on("click", ()=>{
-    $(signInContainer).css('visibility', function(i, v) {
-        return v == 'hidden' ? 'visible' : 'hidden';
+const tryButton = document.getElementById("try");
+const tryButton2 = document.getElementById("try2");
+const messageBox = document.getElementById("messageBox");
+const logoutBtn = document.getElementById("logout");
+
+$(tryButton2).on("click", () => {
+    $(signInContainer).css("visibility", function (i, v) {
+        return v == "hidden" ? "visible" : "hidden";
     });
-})
+});
 
 let signInContainer = document.getElementsByClassName("login_container")[0];
-$(tryButton).on("click", ()=>{
-    $(signInContainer).css('visibility', function(i, v) {
-        return v == 'hidden' ? 'visible' : 'hidden';
+$(tryButton).on("click", () => {
+    $(signInContainer).css("visibility", function (i, v) {
+        return v == "hidden" ? "visible" : "hidden";
     });
-})
-$('#exit_wrapper').on("click", ()=>{
-    $(signInContainer).css('visibility', function(i, v) {
-        return v == 'hidden' ? 'visible' : 'hidden';
-    });
-})
-
-$("#logout").on("click", ()=>{
-    logout();
-})
-
-oktaSignIn.authClient.token.getUserInfo().then(function(user) {
-    document.getElementById("messageBox").innerHTML = "Hello " + `<span style="font-weight:bold">${user.name}</span>` + "! Have a great day.";
-    document.getElementById("logout").style.display = 'flex';
-    document.getElementById("try").style.display = 'none';
-    document.getElementById("try2").style.display = 'none';
-
-}, function(error) {
-    oktaSignIn.showSignInToGetTokens({
-        el: '#osw-container',
-        scopes: ['openid', 'profile']
-    }).then(function(tokens) {
-        oktaSignIn.authClient.tokenManager.setTokens(tokens);
-        oktaSignIn.remove();
-
-        const idToken = tokens.idToken;
-        document.getElementById("messageBox").innerHTML = "Welcome " + `<span style="font-weight:bold">${idToken.claims.name}</span>` + "! You just logged in! :)";
-        document.getElementById("logout").style.display = 'flex';
-        document.getElementById("try").style.display = 'none';
-        tryButton2.style.display = 'none';
-        $(signInContainer).css('visibility', function(i, v) {
-            return v == 'hidden' ? 'visible' : 'hidden';
-        });
-        document.getElementById("todo_title").scrollIntoView();
-    }).catch((err) => console.error(err))
 });
+$("#exit_wrapper").on("click", () => {
+    $(signInContainer).css("visibility", function (i, v) {
+        return v == "hidden" ? "visible" : "hidden";
+    });
+});
+
+$("#logout").on("click", () => {
+    logout();
+});
+
+oktaSignIn.authClient.token.getUserInfo().then(
+    function (user) {
+        showInitialLoggedInUI(user);
+    },
+    function () {
+        oktaSignIn
+            .showSignInToGetTokens({
+                el: "#osw-container",
+                scopes: ["openid", "profile"],
+            })
+            .then(function (tokens) {
+                oktaSignIn.authClient.tokenManager.setTokens(tokens);
+                oktaSignIn.remove();
+
+                const idToken = tokens.idToken;
+                showLoggedInUI(idToken);
+            })
+            .catch((err) => console.error(err));
+    }
+);
 function logout() {
     oktaSignIn.authClient.signOut();
     location.reload();
 }
 
 let table = document.getElementById("tasks_table");
-fetch("/tasks")
-.then((res)=>res.json()
-.then((data)=>{
-    for(entry in data){
-            let tr = document.createElement('tr');
-            let name = document.createElement('td');
+fetch("/tasks").then((res) =>
+    res.json().then((data) => {
+        for (entry in data) {
+            let tr = document.createElement("tr");
+            let name = document.createElement("td");
             name.innerText = data[entry].name;
-            let deleteBtn = document.createElement('td');
-            deleteBtn.innerText = 'Delete';
-            deleteBtn.setAttribute('id',data[entry].id);
-            deleteBtn.setAttribute('class',"remove_btn");
-            setRemoveEntry(deleteBtn,data[entry].id);
+            let deleteBtn = document.createElement("td");
+            deleteBtn.innerText = "Delete";
+            deleteBtn.setAttribute("id", data[entry].id);
+            deleteBtn.setAttribute("class", "remove_btn");
+            setRemoveEntry(deleteBtn, data[entry].id);
 
-            let description = document.createElement('td');
+            let description = document.createElement("td");
             description.innerText = data[entry].description;
 
-            let owner = document.createElement('td');
+            let owner = document.createElement("td");
             owner.innerText = data[entry].owner;
             tr.appendChild(name);
             tr.appendChild(description);
             tr.appendChild(owner);
-            tr.appendChild(deleteBtn)
+            tr.appendChild(deleteBtn);
             table.appendChild(tr);
-    }
-}))
+        }
+    })
+);
 
-$('#btn').on("click", ()=>{
+$("#btn").on("click", () => {
     const accessToken = oktaSignIn.authClient.getAccessToken();
     if (!accessToken) {
-        alert("Please sign in to start interacting with the app")
+        alert("Please sign in to start interacting with the app");
         return;
     } else {
-        let tName = document.getElementById('tname').value;
-        let tDescription = document.getElementById('tDescription').value;
-        if(tName !== "" && tDescription !== ""){
+        let tName = document.getElementById("tname").value;
+        let tDescription = document.getElementById("tDescription").value;
+        if (tName !== "" && tDescription !== "") {
             fetch("/tasks", {
-                method: 'POST',
-                body: JSON.stringify({"name" : tName, "description": tDescription}),
+                method: "POST",
+                body: JSON.stringify({ name: tName, description: tDescription }),
                 headers: {
-                    'Authorization': accessToken,
-                    'Content-Type': 'application/json',
-                }
-            }).then((data)=>{
-                
-            })
+                    Authorization: accessToken,
+                    "Content-Type": "application/json",
+                },
+            }).then((data) => {});
             location.reload();
         }
     }
 });
 
-function setRemoveEntry(btn,id){
-    $(btn).on("click", ()=>{
+function setRemoveEntry(btn, id) {
+    $(btn).on("click", () => {
         const accessToken = oktaSignIn.authClient.getAccessToken();
         if (!accessToken) {
-            alert("Please log in to interact with this beautiful app")
+            alert("Please log in to interact with this beautiful app");
             return;
         } else {
             fetch(`/tasks/${id}`, {
-                method: 'DELETE',
+                method: "DELETE",
                 headers: {
-                    Authorization: accessToken
-                }
-            }).then(res=>{
-                if(res.status === 403){
-                    alert("Only entries created by you can be deleted!")
-                } else {
-                    location.reload();
-                }
-            }).catch(err=>{
-                console.error(err);
-                alert("Entry ID does not exist")
+                    Authorization: accessToken,
+                },
             })
+                .then((res) => {
+                    if (res.status === 403) {
+                        alert("Only entries created by you can be deleted!");
+                    } else {
+                        location.reload();
+                    }
+                })
+                .catch((err) => {
+                    console.error(err);
+                    alert("Entry ID does not exist");
+                });
         }
     });
+}
+function showInitialLoggedInUI(user) {
+    messageBox.innerHTML = "Hello " + `<span style="font-weight:bold">${user.name}</span>` + "! Have a great day.";
+    logoutBtn.style.display = "flex";
+    tryButton.style.display = "none";
+    tryButton2.style.display = "none";
+}
+
+function showLoggedInUI(idToken) {
+    messageBox.innerHTML =
+        "Welcome " + `<span style="font-weight:bold">${idToken.claims.name}</span>` + "! You just logged in! :)";
+    logoutBtn.style.display = "flex";
+    tryButton.style.display = "none";
+    tryButton2.style.display = "none";
+    $(signInContainer).css("visibility", function (i, v) {
+        return v == "hidden" ? "visible" : "hidden";
+    });
+    document.getElementById("todo_title").scrollIntoView();
 }
